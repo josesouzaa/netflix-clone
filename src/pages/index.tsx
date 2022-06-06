7c3e756386586dbbe18ad6864e7a018de78a644a
josesouzaa <josee.souzaaa@gmail.com>
2022-06-06 20:26:02 -0300

    Setup inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import { Header } from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home - Netflix</title>
+      </Head>
+
+      <Header />
+
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Rows */}
+          {/* Rows */}
+          {/* Rows */}
+          {/* Rows */}
+          {/* Rows */}
+        </section>
+      </main>
+
+      {/* Modal */}
+    </div>
+  )
+}
+
+export default Home
