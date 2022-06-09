1cac08c1801f287b40ac7130982679774a4be9df
josesouzaa <josee.souzaaa@gmail.com>
2022-06-09 18:52:42 -0300

    AuthContext criado

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,116 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User
+} from 'firebase/auth'
+import { auth } from '../../firebase'
+
+import { useRouter } from 'next/router'
+
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react'
+
+interface IAuth {
+  user: User | null
+  signUp: (email: string, password: string) => Promise<void>
+  signIn: (email: string, password: string) => Promise<void>
+  logout: () => Promise<void>
+  error: string | null
+  loading: boolean
+}
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+const AuthContext = createContext({} as IAuth)
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [initialLoading, setInitialLoading] = useState<boolean>(true)
+  const [error, setError] = useState(null)
+  const router = useRouter()
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(user)
+          setLoading(false)
+        } else {
+          setUser(null)
+          setLoading(true)
+          router.push('/login')
+        }
+
+        setInitialLoading(false)
+      }),
+    [auth]
+  )
+
+  async function signUp(email: string, password: string) {
+    setLoading(true)
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user)
+        router.push('/')
+        setLoading(false)
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false))
+  }
+
+  async function signIn(email: string, password: string) {
+    setLoading(true)
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user)
+        router.push('/')
+        setLoading(false)
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false))
+  }
+
+  async function logout() {
+    setLoading(true)
+
+    signOut(auth)
+      .then(() => {
+        setUser(null)
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false))
+  }
+
+  const memoedValue = useMemo(
+    () => ({
+      user,
+      signUp,
+      signIn,
+      loading,
+      logout,
+      error
+    }),
+    [user, loading]
+  )
+
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!initialLoading && children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function login() {
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
+
+export default function Login() {
+  const [login, setLogin] = useState<boolean>(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if (login) {
+      // await signIn(email, password)
+    } else {
+      // await signUp(email, password)
+    }
+  }
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -24,26 +49,61 @@ export default function login() {
         alt="Netflix"
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
 
         <div className="space-y-4">
-          <label className="block w-full">
-            <input type="email" placeholder="Email" className="input" />
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: true })}
+            />
+
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
 
-          <label className="block w-full">
-            <input type="password" placeholder="Password" className="input" />
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register('password', { required: true })}
+            />
+
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
 
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          type="submit"
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+        >
           Sign In
         </button>
 
-        <div>
+        <div className="text-[gray]">
           New to Netflix?{' '}
-          <button className="text-white hover:underline">Sign up now</button>
+          <button
+            onClick={() => setLogin(false)}
+            type="submit"
+            className="text-white hover:underline"
+          >
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
